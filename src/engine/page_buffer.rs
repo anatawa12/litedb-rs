ba8a75c6442b768639dc30e34a4ff33147b9014d
anatawa12 <anatawa12@icloud.com>
2025-01-22 14:48:10 +0900

    feat: add helpers for read in page_buffer

--- src/engine/page_buffer.rs
+++ src/engine/page_buffer.rs
@@ -81,3 +81,52 @@ impl PageBuffer {
         // Interlocked.Exchange(ref page.Timestamp, DateTime.UtcNow.Ticks)
     }
 }
+
+// TODO: BufferSlice
+impl PageBuffer {
+    pub fn read_bool(&self, offset: usize) -> bool {
+        self.buffer[offset] != 0
+    }
+
+    pub fn read_byte(&self, offset: usize) -> u8 {
+        self.buffer[offset]
+    }
+
+    pub fn read_i16(&self, offset: usize) -> i16 {
+        i16::from_le_bytes(self.buffer[offset..][..2].try_into().unwrap())
+    }
+
+    pub fn read_i32(&self, offset: usize) -> i32 {
+        i32::from_le_bytes(self.buffer[offset..][..4].try_into().unwrap())
+    }
+
+    pub fn read_i64(&self, offset: usize) -> i64 {
+        i64::from_le_bytes(self.buffer[offset..][..8].try_into().unwrap())
+    }
+
+    pub fn read_u16(&self, offset: usize) -> u16 {
+        u16::from_le_bytes(self.buffer[offset..][..2].try_into().unwrap())
+    }
+
+    pub fn read_u32(&self, offset: usize) -> u32 {
+        u32::from_le_bytes(self.buffer[offset..][..4].try_into().unwrap())
+    }
+
+    pub fn read_u64(&self, offset: usize) -> u64 {
+        u64::from_le_bytes(self.buffer[offset..][..8].try_into().unwrap())
+    }
+
+    pub fn read_f64(&self, offset: usize) -> f64 {
+        f64::from_le_bytes(self.buffer[offset..][..8].try_into().unwrap())
+    }
+
+    // TODO: Decimal
+
+    pub fn read_bytes(&self, offset: usize, length: usize) -> &[u8] {
+        &self.buffer[offset..][..length]
+    }
+
+    pub fn read_string(&self, offset: usize, length: usize) -> Result<&str> {
+        std::str::from_utf8(self.read_bytes(offset, length)).map_err(Error::err)
+    }
+}
