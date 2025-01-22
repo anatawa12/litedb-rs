502af6c61cd33c942ed651ab9a238c58bae78f18
anatawa12 <anatawa12@icloud.com>
2025-01-22 13:30:34 +0900

    chore: add package readme

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+/*!
+ *! # LiteDB in Rust
+ *! This is a reimplementation of [LiteDB] in Rust.
+ *!
+ *! This implementation (currently?) only supports single-threaded operation.
+ *!
+ *! [LiteDB]: https://www.litedb.org/
+ */
