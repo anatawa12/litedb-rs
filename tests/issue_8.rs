051509b0b2985ce3a860c902e05940ac8c85974a
anatawa12 <anatawa12@icloud.com>
2025-11-14 13:30:33 +0900

    test: add test for issue 8: Unexpected detected loop error

---
+++ tests/issue_8.rs
@@ -0,0 +1,22 @@
+use vrc_get_litedb::bson::Value;
+use vrc_get_litedb::document;
+use vrc_get_litedb::file_io::{BsonAutoId, LiteDBFile};
+
+#[test]
+fn issue_8() {
+    let data = include_bytes!("issue_8.liteDb");
+    let file = LiteDBFile::parse(data).unwrap();
+
+    assert_eq!(file.get_by_index("test", "_id", &Value::Int32(0)).collect::<Vec<_>>().len(), 1);
+    assert_eq!(file.get_by_index("test", "_id", &Value::Int32(1)).collect::<Vec<_>>().len(), 1);
+    assert_eq!(file.get_by_index("test", "_id", &Value::Int32(2)).collect::<Vec<_>>().len(), 1);
+    assert_eq!(file.get_by_index("test", "_id", &Value::Int32(3)).collect::<Vec<_>>().len(), 1);
+}
+
+#[test]
+fn issue_8_insert() {
+    let data = include_bytes!("issue_8.liteDb");
+    let mut file = LiteDBFile::parse(data).unwrap();
+
+    file.insert("test", vec![document!{"_id" => 1.5}], BsonAutoId::ObjectId).unwrap();
+}
